0ae0ee030ab0690ae0e71e46e912eb68fddf4f8c
sour-d <36912347+sour-d@users.noreply.github.com>
2022-07-11 09:26:12 +0530

    added redirection if user already logged in

--- src/handler/loginHandler.js
+++ src/handler/loginHandler.js
@@ -1,12 +1,22 @@
+const redirectBack = (response, location) => {
+  response.statusCode = 302;
+  response.method = 'GET';
+  response.setHeader('Location', location);
+  response.end();
+};
+
 const createLoginHandler = serveFileConten =>
   (req, res, sessions) => {
+    if (req.session) {
+      redirectBack(res, '/guest-book');
+      return;
+    }
     if (req.method === 'GET') {
       req.url.pathname = '/login.html';
       serveFileConten(req, res);
       return;
     }
     const name = req.body.get('name');
-    console.log(name);
     if (name) {
       const sessionId = sessions.create({ name });
       res.statusCode = 302;
