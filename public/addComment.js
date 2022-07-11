2fefa644ddc34dcd8a5551d7a9e5423ced11fc1e
sour-d <36912347+sour-d@users.noreply.github.com>
2022-07-11 11:03:09 +0530

    added check for xhr

--- public/addComment.js
+++ public/addComment.js
@@ -1,22 +1,30 @@
 const addComment = () => {
   const form = document.querySelector('form');
-  disableFields(form);
-
   const formData = new FormData(form);
   const body = new URLSearchParams(formData).toString();
-  sendRequest(body);
+
+  disableFields(form);
+  sendRequest(body, onload);
 };
 
 const disableFields = (form) => {
   for (const field of form.elements) {
     field.setAttribute('disabled', 'disabled');
   }
-}
+};
 
 const sendRequest = (body, onload = () => { }) => {
   const xhr = new XMLHttpRequest();
   xhr.open('POST', '/add-comment');
   xhr.send(body);
 
-  xhr.onload = onload;
+  xhr.onload = () => onload(xhr);
 };
+
+const onload = (res) => {
+  console.log(res);
+  if (res.status !== 200) {
+    console.log('Request is not successfull.');
+  }
+  console.log('Sucessful');
+}
